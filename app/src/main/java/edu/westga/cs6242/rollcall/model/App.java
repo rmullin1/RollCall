4a1cc71094eddb6e3413b928afd6d752466cc9cd
R W Mullins <rmullin1@my.westga.edu>
2016-04-13 14:56:08 -0400

    Create App module for context to model

---
+++ app/src/main/java/edu/westga/cs6242/rollcall/model/App.java
@@ -0,0 +1,34 @@
+package edu.westga.cs6242.rollcall.model;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Class App
+ *      This class provides one of several ways to share information between the View and the Model,
+ * in this case, the Context which is required to access any resources under /res or /assets.
+ * This is similar to the Application class static methods in C#.
+ */
+
+//define a class to extend Application so it gets the Context
+public class App extends Application {
+
+    private static Context _context;
+
+    /**
+     * record the context in onCreate() and make it statically available.
+     * Note: this is triggered only for live and instrumentation testing
+     */
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        _context = this;
+    }//onCreate
+
+    //static access routine to Context
+    public static Context getContext() {
+        return _context;
+    }//getContext()
+
+}//class App
+
