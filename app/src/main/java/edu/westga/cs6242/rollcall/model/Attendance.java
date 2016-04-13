4f2c7e4d047f2e9355a05db86872178caae3dfbc
R W Mullins <rmullin1@my.westga.edu>
2016-04-13 14:55:35 -0400

    Rename AttendanceRecord to Attendance to match database

---
+++ app/src/main/java/edu/westga/cs6242/rollcall/model/Attendance.java
@@ -0,0 +1,75 @@
+package edu.westga.cs6242.rollcall.model;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * class Attendance
+ */
+public class Attendance {
+    public int attendanceNo;
+    public Date attendanceDate;
+    public SchoolClass schoolClass;
+    public Student student;
+    public boolean wasPresent;
+
+    /**
+     * ACCESSSORS
+     */
+
+    public int getAttendanceNo() {
+        return this.attendanceNo;
+    }
+
+    public void setAttendanceNo(int value) {
+        this.attendanceNo = value;
+    }
+
+    public Date getAttendanceDate() {
+        return this.attendanceDate;
+    }
+
+    public void setAttendanceDate(Date value) {
+        this.attendanceDate = value;
+    }
+
+    public Student getStudent() {
+        return this.student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public SchoolClass getSchoolClass() {
+        return this.schoolClass;
+    }
+
+    public void setSchoolClass(SchoolClass value) {
+        this.schoolClass = value;
+    }
+
+    public boolean getWasPresent() {
+        return this.wasPresent;
+    }
+
+    public void setWasPresent(boolean value) {
+        this.wasPresent = value;
+    }
+
+    /**
+     * toString()
+     *      Override of Object.toString() to write out Attendance instance as string
+     * @return - string representation of Attendance Record
+     */
+    @Override
+    public String toString() {
+        return String.format("RecordNo: %d, Date: %s, Class: %s, Student: %s, %s",
+                this.attendanceNo,
+                new SimpleDateFormat("yyyy-MM-dd HH:mm").format(this.attendanceDate),
+                this.schoolClass.getClassName(),
+                this.student.getStudentName(), this.wasPresent ? "Present" : "Absent");
+    }
+
+
+}//class
